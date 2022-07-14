8f87b7df69acf18feeed00825b8c183b2651258e
matheus1810 <matheussouza1810@gmail.com>
2022-07-14 00:09:31 -0300

    pagina amarela pronta

--- src/pages/YellowPage/YellowPage.js
+++ src/pages/YellowPage/YellowPage.js
@@ -1,7 +1,10 @@
-import { FaixaDeAviso, IconContainer, PageContainer } from "./styled"
+import { Aparentemente, Box, CheckList, Conhecer, Content, Duvida, DuvidaT, FaixaDeAviso, IconContainer, ImageContainer, Interessante, Items, PageContainer, PerdendoEspaco, SectionWhite, Text, Txt } from "./styled"
 import logoverde from "../../assets/logoverde.png"
 import yellowicon from "../../assets/yellowicon.png"
 import { Footer } from "../../components/Footer/Footer"
+import check from "../../assets/check.png"
+import reload from "../../assets/reload.png"
+import computer from "../../assets/computer.png"
 
 export const YellowPage = () => {
     return (
@@ -21,10 +24,77 @@ export const YellowPage = () => {
                     <img src={yellowicon} />
                 </div>
 
-
             </IconContainer>
 
-         {/*    <Footer/> */}
+            <Aparentemente>Aparentemente seu ERP ainda está conseguindo te atender, mas é importante estar sempre atento aos sinais.</Aparentemente>
+
+            <CheckList>
+                <Txt>
+                    <p>O seu sistema de gestão precisa acompanhar a sua evolução, ou seja, <strong>é ele quem trabalha para você, e nunca o contrário.</strong> </p>
+                    <p> O mínimo que ele deve fazer por você é:</p>
+                </Txt>
+
+                <Items>
+                    <p>
+                        <img
+                            src={check}
+                        />Agilizar os processos internos de controle e gestão</p>
+                    <p>
+                        <img
+                            src={check}
+                        />Permitir que os seus colaboradores tenham mais tempo para pensar de forma mais estratégica e em novas soluções</p>
+                    <p>
+                        <img
+                            src={check}
+                        />Facilitar a execução de tarefas e rotinas de trabalho</p>
+                    <p>
+                        <img
+                            src={check}
+                        />Garantir dados confiáveis e em tempo real, utilizando cada vez menos planilhas</p>
+                    <p>
+                        <img
+                            src={check}
+                        />Exigir quase nenhuma integração com outros softwares de mercado.</p>
+                </Items>
+            </CheckList>
+
+            <SectionWhite>
+
+                <Interessante>
+                    É interessante também pensar sempre de modo holístico. Como tem sido a gestão da sua empresa no geral? Você já comparou a  execução dos seus processos com a dos seus concorrentes, por exemplo?
+                </Interessante>
+
+                <PerdendoEspaco>
+                    <Text>
+                        <p> Será que você está perdendo espaço no mercado por não ter processos bem definidos e executados, ou por falta de informação para a tomada de decisão? </p>
+                        <br />
+                        <p> Se essa reflexão te interessa, te convido a conhecer uma ferramenta completa de diagnóstico de maturidade de gestão que tem o objetivo de te mostrar as falhas nos seus processos e como você está em relação aos seus concorrentes.</p>
+                    </Text>
+                    <Box>
+                        <img src={reload} />
+                        <p>Utilizamos o Maha Gestão, uma ferramenta com mais de XXX empresas cadastradas e totalmente gratuita.</p>
+                        <button>Solicitar o diagóstico de gestão</button>
+                    </Box>
+                </PerdendoEspaco>
+            </SectionWhite>
+
+            <Conhecer>
+                <Content>
+                    <p>Caso você queira conhecer melhor a nossa solução, é só clicar aqui embaixo para agendar uma reunião com um dos nossos consultores:</p>
+                    <button>Fale com nossos consultores</button>
+                </Content>
+            </Conhecer>
+
+            <Duvida>
+                <ImageContainer>
+                    <img src={computer} />
+                </ImageContainer>
+                <DuvidaT>
+                    <p>Ainda tem alguma dúvida sobre a Troca de ERP? Você pode baixar o nosso guia completo:</p>
+                    <button>Guia completo da troca de ERP + Checklist</button>
+                </DuvidaT>
+            </Duvida>
+            <Footer />
 
         </PageContainer>
     )
--- src/pages/YellowPage/styled.js
+++ src/pages/YellowPage/styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 export const PageContainer = styled.div`
+background:#F3F1F0;
 .img-container{
     display:flex;
     align-items:center;
@@ -28,7 +29,6 @@ export const FaixaDeAviso = styled.div`
     color: #2E3C50;
 }
 .alerta{
-    width: 680px;
     height: 45px;
     font-family:sans-serif;
     font-style: normal;
@@ -41,6 +41,7 @@ export const FaixaDeAviso = styled.div`
 }
 `
 export const IconContainer=styled.div`
+
 width:100vw;
 position:absolute;
 .circle{
@@ -55,4 +56,224 @@ position:absolute;
     top:85%;
     left:50%;
 }
+`
+export const Aparentemente=styled.div`
+
+    font-family:sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 28px;
+    text-align: center;
+    color: #333333;
+    margin:200px auto 80px auto  ;
+    width:600px;
+    @media (max-width:700px) {
+        width:80%;
+    
+    }
+
+
+`
+
+export const CheckList=styled.section`
+display:flex;
+flex-wrap:wrap;
+width:80vw;
+margin:0 auto ;
+justify-content:space-around;
+`
+export const Txt=styled.div`
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 26px;
+    width:300px;
+    color: #4D4D4D;
+    padding:40px;
+
+`
+export const Items=styled.div`
+width:40%;
+@media (max-width:791px) {
+    width:90%;
+    
+    }
+p{
+    padding:10px;
+    font-family: sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color: #4D4D4D;
+
+}
+`
+
+export const SectionWhite=styled.div`
+background-color:#ffffff;
+`
+
+export const Interessante=styled.div`
+
+    font-family:sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 28px;
+    text-align: center;
+    color: #333333;
+    margin:50px auto ;
+    padding-top:50px;
+    width:50vw;
+
+`
+
+export const PerdendoEspaco=styled.div`
+display:flex;
+flex-wrap:wrap;
+width:70vw;
+margin:0 auto ;
+justify-content: space-around;
+`
+export const Text =styled.div`
+    width:300px;
+    font-family: 'Open Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    color: #4D4D4D;
+    margin-bottom:50px;
+
+`
+export const Box =styled.div`
+    width:400px;
+    display:flex;
+    padding:30px;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    background: linear-gradient(180deg, #525D6E 65.95%, #77808C 100%);
+button{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 20px;
+    width: 311px;
+    height: 36px;
+    background: #66CB66;
+    border:none;
+
+
+    font-family: sans-serif;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    text-align: center;
+    color: #2E3C50;
+}
+p{
+    font-family:sans-serif;
+    font-style: normal;
+    font-size: 22px;
+    line-height: 28px;
+    text-align: center;
+    padding:30px;
+    color: #FFFFFF;
+}
+`
+export const Conhecer=styled.section`
+background-color:#2E3C50;
+
+`
+export const Content=styled.div`
+display:flex;
+flex-direction:column ;
+justify-content:center ;
+align-items:center;
+padding:50px;
+p{
+    font-family:sans-serif;
+    font-style: normal;
+    font-size: 24px;
+    line-height: 30px;
+    text-align: center;
+    color: #F3F1F0;
+    max-width:600px;
+}
+button{
+    height:40px;
+    background-color:transparent;
+    border:3px solid #66CB66;
+    color: #66CB66;
+    font-family:sans-serif;
+    font-style: normal;
+    font-size: 16px;
+    line-height: 20px;
+    text-align: center;
+    width:250px;
+    
+    margin-top:30px;
+
+}
+`
+export const Duvida=styled.div`
+width:80vw;
+margin:0 auto  ;
+display:flex ;
+padding-top:50px ;
+padding-bottom:50px;
+@media (max-width:1150px) {
+    flex-wrap:wrap;
+    
+    }
+`
+export const DuvidaT=styled.div`
+
+p{
+    font-family: sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 30px;
+    color: #525D6E;
+    margin-top:50px;
+
+}
+button{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 20px;
+    gap: 10px;
+    height: 36px;
+    background: #66CB66;
+    border:none;
+    font-family: 'Sora';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    text-align: center;
+    color: #2E3C50;
+    margin:50px auto;
+
+}
+`
+export const ImageContainer = styled.div`
+img{
+    width:100%;
+    height:100%;
+}
+@media (max-width:700px) {
+    width:80vw;
+    
+    }
+
 `
